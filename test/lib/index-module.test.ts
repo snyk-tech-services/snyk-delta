e2e87874a8de0f14bc27c9f18a36e5a37dbdba99
aarlaud <antoine@snyk.io>
2020-05-17 23:00:22 +0200

    Return codes when using module and update test cases

--- test/lib/index-module.test.ts
+++ test/lib/index-module.test.ts
@@ -1,14 +1,8 @@
-import { stdin, MockSTDIN } from 'mock-stdin';
-import { mockProcessExit } from 'jest-mock-process';
 import * as nock from 'nock';
 import * as path from 'path';
 import * as fs from 'fs';
 //process.argv.push('-d');
-process.argv.push('--baselineOrg=playground');
-process.argv.push('--baselineProject=c51c80c2-66a1-442a-91e2-4f55b4256a72');
 
-const stdinMock: MockSTDIN = stdin();
-const mockExit = mockProcessExit();
 import { getDelta } from '../../src/lib/index';
 
 const fixturesFolderPath = path.resolve(__dirname, '..') + '/fixtures/';
@@ -20,9 +14,6 @@ const mockedLog = (output: string): void => {
 };
 beforeAll(() => {
   console.log = mockedLog;
-});
-afterEach(() => {
-  stdinMock.reset();
 });
 
 beforeEach(() => {
@@ -68,38 +59,26 @@ beforeEach(() => {
     });
 });
 
-describe('Test End 2 End - Inline mode with project coordinates', () => {
-  it('Test Inline mode with specified project coordinates - no new issue', async () => {
-    setTimeout(() => {
-      stdinMock.send(
-        fs
-          .readFileSync(fixturesFolderPath + 'snykTestsOutputs/test-goof.json')
-          .toString(),
-      );
-      stdinMock.send(null);
-    }, 100);
-
-    const result = await getDelta();
+describe('Test End 2 End - Module', () => {
+  it('Test module mode - no new issue', async () => {
+    const result = await getDelta(
+      fs
+        .readFileSync(fixturesFolderPath + 'snykTestsOutputs/test-goof.json')
+        .toString(),
+    );
     expect(consoleOutput).toContain('No new issues found !');
-    // => When testing, loaded as module therefore returning code === process.exitCode
-    // expect(mockExit).toHaveBeenCalledWith(0);
     expect(result).toEqual(0);
   });
 
-  it('Test Inline mode with specified project coordinates - 1 new issue', async () => {
-    setTimeout(() => {
-      stdinMock.send(
-        fs
-          .readFileSync(
-            fixturesFolderPath +
-              'snykTestsOutputs/test-goof-with-one-more-vuln.json',
-          )
-          .toString(),
-      );
-      stdinMock.send(null);
-    }, 100);
-
-    const result = await getDelta();
+  it('Test module mode - 1 new issue', async () => {
+    const result = await getDelta(
+      fs
+        .readFileSync(
+          fixturesFolderPath +
+            'snykTestsOutputs/test-goof-with-one-more-vuln.json',
+        )
+        .toString(),
+    );
 
     const expectedOutput = [
       'New issue introduced !',
@@ -113,8 +92,6 @@ describe('Test End 2 End - Inline mode with project coordinates', () => {
     expectedOutput.forEach((line: string) => {
       expect(consoleOutput.join()).toContain(line);
     });
-    // => When testing, loaded as module therefore returning code === process.exitCode
-    // expect(mockExit).toHaveBeenCalledWith(1);
     expect(result).toEqual(1);
   });
 });
